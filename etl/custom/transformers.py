4ae7b493cdbc3bd04348b94b7b4fbee01a4d8c80
alvin <alvin@clarifyhealth.com>
2019-04-24 13:19:46 -0700

    adding more test

--- etl/custom/transformers.py
+++ etl/custom/transformers.py
@@ -1,7 +1,7 @@
 from pyspark import keyword_only
 from pyspark.ml import Transformer
 from pyspark.ml.param.shared import HasInputCol, HasOutputCol
-from pyspark.sql.functions import udf, expr, pandas_udf
+from pyspark.sql.functions import udf, expr, pandas_udf, split, size
 from pyspark.sql.types import LongType
 
 
@@ -68,3 +68,22 @@ class MyWordCounterVectorizedUDF(Transformer, HasInputCol, HasOutputCol):
             return s.str.split(" ").str.len()
 
         return dataset.withColumn(out_col, word_count(in_col))
+
+
+class MyWordCounterBuiltInUDF(Transformer, HasInputCol, HasOutputCol):
+    @keyword_only
+    def __init__(self, inputCol=None, outputCol=None):
+        super(MyWordCounterBuiltInUDF, self).__init__()
+        kwargs = self._input_kwargs
+        self.setParams(**kwargs)
+
+    @keyword_only
+    def setParams(self, inputCol=None, outputCol=None):
+        kwargs = self._input_kwargs
+        return self._set(**kwargs)
+
+    def _transform(self, dataset):
+        out_col = self.getOutputCol()
+        in_col = dataset[self.getInputCol()]
+
+        return dataset.withColumn(out_col, size(split(in_col, " ")))
--- tests/test_custom_transformations.py
+++ tests/test_custom_transformations.py
@@ -1,6 +1,7 @@
 import pytest
 
-from etl.custom.transformers import MyWordCounterPyUDF, MyWordCounterScalaUDF, MyWordCounterVectorizedUDF
+from etl.custom.transformers import MyWordCounterPyUDF, MyWordCounterScalaUDF, MyWordCounterVectorizedUDF, \
+    MyWordCounterBuiltInUDF
 from pyspark.sql import Row
 
 
@@ -78,6 +79,34 @@ def test_vectorized_udf_transformer(spark_session, input_data):
     df = spark_session.createDataFrame(input_data)
 
     tr = MyWordCounterVectorizedUDF(inputCol="input_col", outputCol="output_col")
+
+    tdf = tr.transform(df)
+
+    tdf.explain()
+
+    tdf.show()
+
+    assert tdf.count() == 6
+
+
+@pytest.mark.parametrize('input_data',
+                         [
+                             (
+                                     [
+                                         Row(input_col="Hello this is my favourite test"),
+                                         Row(input_col="This is cool"),
+                                         Row(input_col="Time for some performance test"),
+                                         Row(input_col="Clarify Tera Team"),
+                                         Row(input_col="Doing things right and doing the right thing"),
+                                         Row(input_col="Oh Model fit and predict")
+                                     ]
+                             )
+
+                         ])
+def test_built_in_udf_transformer(spark_session, input_data):
+    df = spark_session.createDataFrame(input_data)
+
+    tr = MyWordCounterBuiltInUDF(inputCol="input_col", outputCol="output_col")
 
     tdf = tr.transform(df)
 
